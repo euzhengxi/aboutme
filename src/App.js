04a39ed0b91b6c87241aa7c490c2b12f26e9aeb2
euzhengxi <euzhengxi110@gmail.com>
2023-01-17 23:00:29 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Typewriter from 'typewriter-effect';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Typewriter
+  options={{
+    strings: ['Hello', 'World'],
+    autoStart: true,
+    loop: true,
+  }}
+/>
     </div>
   );
 }
